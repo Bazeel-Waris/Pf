2823545a2694e99b13af70bd70738f3df1b90606
Bazeel-Waris <ahmadbinwaris@gmail.com>
2023-09-14 13:37:44 +0500

    Fix line endings to LF

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        DB::table('test')->insert([
+            'title' => Str::random(10),
+            'content' => Str::random(50),
+        ]);
+    }
+}
